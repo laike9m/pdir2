a3455f844fec919616de85f5751f4b8db6f1635e
laike9m <laike9m@gmail.com>
2017-07-01 16:38:59 +0800

    Test Python 3.5 (#30)

--- tests/test_pdir_format.py
+++ tests/test_pdir_format.py
@@ -86,7 +86,7 @@ def test_pdir_class():
                 '\x1b[0m\x1b[0;36m__module__\x1b[0m\x1b[1;30m, '
                 '\x1b[0m\x1b[0;36m__weakref__\x1b[0m')
         ])
-    else:
+    elif sys.version_info[:2] == (3, 6):
         assert result.repr_str == repr(result) == '\n'.join([
             '\x1b[0;33mabstract class:\x1b[0m',
             '    \x1b[0;36m__subclasshook__\x1b[0m',
@@ -97,6 +97,40 @@ def test_pdir_class():
                 '\x1b[0m\x1b[0;36m__setattr__\x1b[0m'),
             '\x1b[0;33mclass customization:\x1b[0m',
             '    \x1b[0;36m__init_subclass__\x1b[0m',
+            '\x1b[0;33mobject customization:\x1b[0m', (
+                '    \x1b[0;36m__format__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__hash__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__init__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__new__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__repr__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__sizeof__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__str__\x1b[0m'),
+            '\x1b[0;33mpickle:\x1b[0m', (
+                '    \x1b[0;36m__reduce__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__reduce_ex__\x1b[0m'),
+            '\x1b[0;33mrich comparison:\x1b[0m', (
+                '    \x1b[0;36m__eq__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__ge__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__gt__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__le__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__lt__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__ne__\x1b[0m'),
+            '\x1b[0;33mspecial attribute:\x1b[0m', (
+                '    \x1b[0;36m__class__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__dict__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__doc__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__module__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__weakref__\x1b[0m')
+        ])
+    else:
+        assert result.repr_str == repr(result) == '\n'.join([
+            '\x1b[0;33mabstract class:\x1b[0m',
+            '    \x1b[0;36m__subclasshook__\x1b[0m',
+            '\x1b[0;33mattribute access:\x1b[0m', (
+                '    \x1b[0;36m__delattr__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__dir__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__getattribute__\x1b[0m\x1b[1;30m, '
+                '\x1b[0m\x1b[0;36m__setattr__\x1b[0m'),
             '\x1b[0;33mobject customization:\x1b[0m', (
                 '    \x1b[0;36m__format__\x1b[0m\x1b[1;30m, '
                 '\x1b[0m\x1b[0;36m__hash__\x1b[0m\x1b[1;30m, '
