cd5ec7b2953c294abe356914e0fa40aeb148a92e
laike9m <laike9m@gmail.com>
2017-10-15 02:11:46 +0800

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ history = codecs.open('docs/HISTORY.rst', encoding='utf-8').read()
 
 setup(
     name='pdir2',
-    version='0.2.1',
+    version='0.2.2',
     description='Pretty dir printing with joy',
     long_description=u'\n\n'.join([readme, history]),
     author='laike9m',
