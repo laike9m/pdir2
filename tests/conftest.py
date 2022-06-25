199531c5e39a19baa8a55d92c730fcef2812dacf
laixintao <laixintaoo@gmail.com>
2022-06-25 13:13:18 +0800

    auto use tty fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def tty():
     with patch("sys.stdout.isatty") as faketty:
         faketty.return_value = True
